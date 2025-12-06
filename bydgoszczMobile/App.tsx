6da74a0c4f87774c5c3462159c6ad6dd09d4d8df
Mikołaj Pietroń <Mikusiek@MacBook-Air-Aleksandra.local>
2025-12-06 14:54:45 +0100

    jol

--- bydgoszczMobile/App.tsx
+++ bydgoszczMobile/App.tsx
@@ -1,11 +1,13 @@
+import './global.css';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App component rendering...");
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Witaj w bydgoszczMobile! 🎉
+        Witaj w bydgoszczMobile kuaaarwaaaaaarrr! 🎉
       </Text>
       <StatusBar style="auto" />
     </View>
--- bydgoszczMobile/tailwind.config.js
+++ bydgoszczMobile/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}'
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {}
   },
